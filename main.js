55178a60d045ec2adabf87a2cacabe4cf7578024
Schwalbe262 <41924534+Schwalbe262@users.noreply.github.com>
2020-07-26 22:46:33 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1303,17 +1303,19 @@ function response(room, msg, sender, isGroupChat, replier, imageDB) {
 
 
 		if(msg.indexOf("/시갤검색글쓴이") == 0){
-			if(DCP.UOS_search(msg.subst(9),"writer") != undefined){
-				r.reply(DCP.UOS_search(msg.subst(9),"writer"))
+			let temp = DCP.UOS_search(msg.substr(9),"writer")
+			if(temp != undefined){
+				r.reply(temp)
 			}
 			else{
 				r.reply("검색 결과가 없습니다.")
 			}
-			
+
 		}
 		else if(msg.indexOf("/시갤검색") == 0){
-			if(DCP.UOS_search(msg.subst(6),"writer") != undefined){
-				r.reply(DCP.UOS_search(msg.substr(6),"title"))
+			let temp = DCP.UOS_search(msg.substr(6),"writer")
+			if(temp != undefined){
+				r.reply(temp)
 			}
 			else{
 				r.reply("검색 결과가 없습니다.")
